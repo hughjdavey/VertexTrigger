6144524dfe1522169bfd807b05b7947d5bc050cc
Hugh Davey <hdavey01@mail.bbk.ac.uk>
2014-10-19 12:20:38 +0100

    set up working project and changed folder names

---
+++ desktop/src/com/VertexTrigger/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.VertexTrigger.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.VertexTrigger.VertexTrigger;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VertexTrigger(), config);
+	}
+}
